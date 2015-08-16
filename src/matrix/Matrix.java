a6c4b3601dc2909032665d55bd48e9edccdfe126
esmeail <esmeail@nabil-PC>
2015-08-16 22:08:35 +0300

    Add subtract function

--- src/matrix/Matrix.java
+++ src/matrix/Matrix.java
@@ -1,35 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package matrix;
 
+/**
+ *
+ * @author ESMAIL
+ */
 public class Matrix {
 
-
+    /**
+     * @param args the command line arguments
+     */
+    public static void Subtract(int[][] A, int[][] B)
+    {
+        
+        for(int row = 0; A.length > row;row++ )
+        {
+            for(int column = 0; A[0].length > column;column++ )
+            {
+                System.out.print(A[row][column] - B[row][column] + " ");
+            }
+        
+            System.out.println();
+        }
     
+    
+    }
     public static void main(String[] args) {
-
-        int aa[][]={{1,2},{1,2}};
-        int bb[][]={{1,2},{1,2}};
-        add(aa,bb);
-        
-        print(aa);
-        
-       
+        // TODO code application logic here
+        int a = 10;
+        int[][] A = new int[10][5];
+        int[][] B = new int[10][5];
+        A[0][0] = 10;
+        B[0][0] = 5;
+        Subtract(A, B);
     }
-    
-    public static void add(int a [][], int b [][]) {
-        for(int i=0;i<a.length;i++){
-            for(int j=0;j<a[0].length;j++){
-                a[i][j] += b[i][j];
-            }
-        }
-    }
-    
-    public static void print(int a [][]) {
-        for(int i=0;i<a.length;i++){
-            for(int j=0;j<a[0].length;j++){
-                System.out.println(a[i][j]);
-            }
-        }
-    }
-    /////
-    
 }
